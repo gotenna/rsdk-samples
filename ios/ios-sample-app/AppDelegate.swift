d5576038f00a98a14e532f06f0bd2518541880b8
Adam Johns <adam@gotenna.com>
2025-04-01 16:01:11 -0500

    cleanup

--- ios/ios-sample-app/AppDelegate.swift
+++ ios/ios-sample-app/AppDelegate.swift
@@ -1,16 +1,7 @@
-//
-//  AppDelegate.swift
-//  ios-sample-app
-//
-//  Created by Adam Johns on 9/25/24.
-//
-
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -31,6 +22,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
 }
-
