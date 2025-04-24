a9ff425c18a210baf7cbdfaf53205fee1afb572a
Huy To <huy@gotenna.com>
2025-04-24 09:27:25 -0700

    fix: Avoid the Android 15 edge-to-edge support for now

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
     defaultConfig {
         applicationId = "com.gotenna.android.rsdksample"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
