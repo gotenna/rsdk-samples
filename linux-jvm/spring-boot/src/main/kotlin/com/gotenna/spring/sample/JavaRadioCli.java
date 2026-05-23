52fbb85ec671b94a5339bdd2cfe84ea5e9957947
Rene <rene@gotenna.com>
2026-05-23 12:51:50 -0400

    RSDK-791 - Update linux sample to Radio SDK 3.4.15
    Bump radio-sdk from 3.3.19 to 3.4.15 in linux-jvm/gradle/libs.versions.toml.
    Add the new sendMeshAddressResolution argument to the GotennaHeaderWrapper
    constructor call in JavaRadioCli.java (Kotlin uses the default value; Java
    callers must specify it explicitly).

--- linux-jvm/spring-boot/src/main/kotlin/com/gotenna/spring/sample/JavaRadioCli.java
+++ linux-jvm/spring-boot/src/main/kotlin/com/gotenna/spring/sample/JavaRadioCli.java
@@ -159,7 +159,8 @@ public class JavaRadioCli {
                                 "senderUUID",
                                 "senderCallsign",
                                 null,
-                                "uuid"
+                                "uuid",
+                                null
                         ),
                         GripResult.Unknown.INSTANCE,
                         null,
